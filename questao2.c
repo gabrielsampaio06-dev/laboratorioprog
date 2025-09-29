fb12fe1699ec5c5bbee882d8b1d9ebb0e11e6d3b
gabrielsampaio06-dev <gabriel.sampaio06@aluno.ifce.edu.br>
2025-09-29 18:21:45 -0300

    arquivos soltos retirados

--- questao2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-    float x;
-    printf("digite um numero: \n");
-    scanf("%f",&x);
-    printf("o numero com duas casas decimais é: %.2f\n",x);
-
-    return 0;
-}
--- quetao13.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-     unsigned x;
-    printf("Digite a temperatura em Celsius: ");
-    scanf("%u",&x);
-    float y = (9*(float)x)+160.0;
-    printf("%u Celsius valem %.2f Fahrenheint", x , y);
-    return 0;
-}
